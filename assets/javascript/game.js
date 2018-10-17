a63b92e1826ba00c8f27ccf93b59abc97a904479
kalaay waaye <kalaay1111@gmail.com>
2018-10-17 17:55:29 -0400

    finalfinal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,122 @@
 $(document).ready(function() {
 
-   var randomNum =0;
-   var blueChrystal;
-   var greenChrystal;
-   var redChrystal;
-   var purpleChrystal;
+   var randomNum = 0;
+   var blueChrystal = 0;
+   var greenChrystal =0;
+   var redChrystal =0;
+   var purpleChrystal =0;
    var wins = 0;
-   var loses = 1;
-   var totalScore = 40;
+   var loses = 0;
+   var totalScore = 0;
 
-   // generate random number at the start of the game 
-   randomNum = Math.floor((Math.random() * 120) + 19);
-   $("#randomNumDislpay").text(randomNum);
+   //generate random number at the start of the game 
+   randomNum = document.getElementById("randomNumDislpay").innerHTML =Math.floor(Math.random() * 101) + 19;
 
-   console.log(randomNum);
-
+    
    blueChrystal = Math.floor((Math.random() * 12) + 1);
    console.log(blueChrystal);
-   $("#bluestone").text(blueChrystal);
 
+   $("#bluestone").on ("click", function(){
+    totalScore = totalScore + blueChrystal;
+    $("#totalScoreDislpay").text(totalScore);
+    console.log(totalScore)
+
+    if (totalScore == randomNum) {
+    winner();
+    }
+    
+    else if (totalScore > randomNum)  {
+        loser();
+        
+    }
+
+})
+   
    greenChrystal = Math.floor((Math.random() * 12) + 1);
    console.log(greenChrystal);
+
+   $("#greenstone").on ("click", function(){
+    totalScore = totalScore + greenChrystal;
+    $("#totalScoreDislpay").text(totalScore);
+    console.log(totalScore)
+    if (totalScore == randomNum) {
+        winner();
+        }
+        
+        else if (totalScore > randomNum)  {
+            loser();
+            
+        }
+})
 
    redChrystal = Math.floor((Math.random() * 12) + 1);
    console.log(redChrystal);
 
+   $("#red").on ("click", function(){
+    totalScore = totalScore + redChrystal;
+    $("#totalScoreDislpay").text(totalScore);
+    console.log(totalScore)
+    if (totalScore == randomNum) {
+        winner();
+        }
+        
+        else if (totalScore > randomNum)  {
+            loser();
+            
+        }
+})
+
    purpleChrystal = Math.floor((Math.random() * 12) + 1);
    console.log(purpleChrystal);
 
-   $("#winsDislpay").text(wins);
+   $("#purple").on ("click", function(){
+    totalScore = totalScore + purpleChrystal;
+    $("#totalScoreDislpay").text(totalScore);
+    console.log(totalScore)
+    if (totalScore == randomNum) {
+        winner();
+        }
+        
+        else if (totalScore > randomNum)  {
+            loser();
+            
+        }
+})
+
+// Displays the number of wins and loses on the screen
+
+   $("#winsDislpay").append (wins);
    console.log(wins);
 
-   $("#losesDislpay").text(loses);
+   $("#losesDislpay").append(loses);
    console.log(loses);
 
-   $("#totalScoreDislpay").text(totalScore);
+  // $("#totalScoreDislpay").append(totalScore);
    console.log(totalScore);
-
-
-
-
 });
+
+function winner(){
+    alert("WINNER-GAGNON!!");
+    win++;
+    $("#winsDislpay").text(wins);
+}
+
+function loser(){
+    alert("YOU LOSE!");
+    loses++;
+    $("#losesDislpay").text(loses);
+}
+
+function compare(){
+
+    if (totalScore == randomNum) {
+        winner();
+        }
+        
+        else if (totalScore > randomNum)  {
+            loser();
+            
+        }
+}
+
+
